9eeb7550272145ff3591bbcfecfdaf40b4dd9edb
Neutralized <Neutralization@users.noreply.github.com>
2019-11-18 18:09:54 +0000

    2019-11-19

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # bilibili-down
 Shell script to download bilibili videos
+
+## Requirements
+
+### Linux
+- [aria2](https://aria2.github.io/)  download videos
+- [awk](http://www.gnu.org/software/gawk/gawk.html)
+- [curl](https://curl.haxx.se/)
+- [ffmpeg](https://www.ffmpeg.org/)  merge video parts
+- [jq](https://stedolan.github.io/jq/)  json support
+- [sed](https://www.gnu.org/software/sed/)
+
+### Windows
+- [Cygwin](https://www.cygwin.com/)
+- [MinGW](http://www.mingw.org/)
+- [WSL/WSL2](https://docs.microsoft.com/en-us/windows/wsl/about)
+
+Choose one of three, and install tools mentioned in linux
+
+## Features
+- Download any bilibili videos with highest quality(*cookies needed*).
+- Only support avid URL for now.
+
+## Install
+- Place bilidown.sh where you like.
+
+## Usage
+- Add alias:
+> alias bilidown='sh /path/to/bilidown.sh'
+
+- Run:
+> bilidown av62401803  
+> bilidown https://www.bilibili.com/video/av62401803  
+> bilidown https://www.bilibili.com/video/av62401803?from=search&seid=15964470553028171854
+
+- VIP only videos
+1. Open `http://www.bilibili.com` and login
+2. Press <kbd>F12</kbd> in Chrome of Firefox
+3. Select `Network`
+4. Press <kbd>F5</kbd> to reload the page
+5. Selct first line and copy your cookies value
+6. Save to `cookies` file as `DedeUserID=****; DedeUserID__ckMd5=****; SESSDATA=****; bili_jct=****`
+7. Place `cookies` file in the same folder where `bilidown.sh` is
+
+## Todo
+- [ ] Customize output folder and filename.
+- [ ] Support epid/ssid/mediaid URL.
+- [ ] Support interaction videos.
+- [x] Mulitple pages download.
+- [x] Mulitple videos download.
+- [x] Support avid URL.
---
+++ bilidown.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+IFS=$'\n'
+if [ $# -eq 0 ]
+then
+    echo -e "->Need AVID to download!\n"
+    exit 1
+fi
+until [ $# -eq 0 ]
+do
+    aid=`echo $1 | sed -e 's/.*av//g' -e 's/[a-zA-Z?/].*//g'`
+    cookies=`cat ./cookies`
+
+    pagelist='https://api.bilibili.com/x/player/pagelist?aid='$aid'&jsonp=jsonp'
+    echo -e "->Getting video list: \n"$pagelist
+    cids=`curl -sL -H "Cookie: "$cookies $pagelist | jq -r '.data[].cid'`
+    title=`curl -sL -H "Cookie: "$cookies 'https://api.bilibili.com/x/web-interface/view?aid='$aid | jq -r '.data.title' | sed 's/[/?!.*|:]//g'`
+    cids_arr=($cids)
+    echo -e "->Found video pages: "${#cids_arr[@]}
+
+    part=0
+    for cid in $cids
+    do
+        episode=$(( ++part ))
+        echo -e "->Download video page: "$episode
+        if [ "${#cids_arr[@]}" == "1" ]
+        then
+            filename=av$aid.$title.mp4
+        else
+            filename=av$aid.$title【P$episode】.mp4
+        fi
+        json_url='https://api.bilibili.com/x/player/playurl?avid='$aid'&cid='$cid'&qn=116&fnver=0&fnval=16&otype=json&type='
+        echo -e "->Getting video source: \n"$json_url    
+        json=`curl -sL -H "Cookie: "$cookies $json_url`
+        dash=`echo $json | jq '.data|has("dash")'`
+        durl=`echo $json | jq '.data|has("durl")'`
+        
+        if [ "$dash" == "true" ]
+        then
+            vp=`echo $json | jq -r '.data.dash.video[0].baseUrl'`
+            ap=`echo $json | jq -r '.data.dash.audio[0].baseUrl'`
+
+            echo -e "->Downloading Video Dash"
+            aria2c -x10 -k1M --file-allocation=none --auto-file-renaming=false --allow-overwrite=true $vp\
+                --show-console-readout false --quiet \
+                --header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0"\
+                --header="Accept: */*"\
+                --header="Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2"\
+                --header="Referer: https://www.bilibili.com/video/av"$aid\
+                --header="Origin: https://www.bilibili.com"\
+                --header="DNT: 1"\
+                --header="Connection: keep-alive"\
+                --header="Pragma: no-cache"\
+                --header="Cache-Control: no-cache"\
+                --header="Cookie: "$cookies \
+                --out ./v_$cid.m4s
+
+            echo -e "->Downloading Audio Dash"
+            aria2c -x10 -k1M --file-allocation=none --auto-file-renaming=false --allow-overwrite=true $ap\
+                --show-console-readout false --quiet \
+                --header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0"\
+                --header="Accept: */*"\
+                --header="Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2"\
+                --header="Referer: https://www.bilibili.com/video/av"$aid\
+                --header="Origin: https://www.bilibili.com"\
+                --header="DNT: 1"\
+                --header="Connection: keep-alive"\
+                --header="Pragma: no-cache"\
+                --header="Cache-Control: no-cache"\
+                --header="Cookie: "$cookies \
+                --out ./a_$cid.m4s
+
+            echo -e "->Merge into file: "$filename
+            ffmpeg -i ./v_$cid.m4s -i ./a_$cid.m4s -c:v copy -c:a copy\
+                -y -hide_banner -loglevel panic \
+                ./$filename
+            echo -e "->Removing temp files\n"
+            rm *.m4s
+        elif [ "$durl" == "true" ]
+        then
+            flvs=`echo $json | jq -r '.data.durl[].url'`
+            for flv in $flvs
+            do
+                flvname=`echo $flv | sed -e 's/\?.*//g' -e 's/.*\///g'`
+                echo "file './"$flvname"'" >> ./merge_$cid.txt
+                echo "->Downloading Video Part: "$flvname
+                aria2c -x10 -k1M --file-allocation=none --auto-file-renaming=false --allow-overwrite=true $flv\
+                    --show-console-readout false --quiet \
+                    --header="User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0"\
+                    --header="Accept: */*"\
+                    --header="Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2"\
+                    --header="Referer: https://www.bilibili.com/video/av"$aid\
+                    --header="Origin: https://www.bilibili.com"\
+                    --header="DNT: 1"\
+                    --header="Connection: keep-alive"\
+                    --header="Pragma: no-cache" --header="Cache-Control: no-cache"\
+                    --header="Cookie: "$cookies \
+                    --out ./$flvname
+            done
+            echo "->Merge into file: "$filename
+            ffmpeg -safe 0 -f concat -i ./merge_$cid.txt -c copy\
+                -y -hide_banner -loglevel panic \
+                ./$filename
+            echo -e "->Removing temp files\n"
+            rm *.flv
+            rm ./merge_$cid.txt
+        else
+            echo -e "->Error: Video not found!\n"
+        fi
+    done
+    shift
+done
